dc994700718ca6aea144f71f7f41efc8a9663533
ansuar5254 <ansuar5254@gmail.com>
2025-10-19 19:15:22 +0300

    this the first Leetcoder pushed to github

---
+++ maximumFrequency.py
@@ -0,0 +1,15 @@
+def maxFrequencyElements(nums):
+         numset = set(nums)
+         freq = []
+         for i in numset:
+              fre = nums.count(i)
+              freq.append(fre)
+         maxfre = max(freq)
+         total = 0
+         for i in freq:
+             if maxfre == i:
+                total +=1
+         return (maxfre * total)
+number  = input("Enter the number separated by space: ")
+nums = number.split() 
+print(maxFrequencyElements(nums))
